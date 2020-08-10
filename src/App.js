53f31c9d26b5551e87259219fd69d22d90b76275
sarita87das <nutankarna87@gmail.com>
2020-08-09 23:10:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import SearchResultContainer from "./components/SearchResultContainer";
+import SearchForm from "./components/SearchForm";
+import ResultList from "./components/ResultList";
+import EmployeeCard from "./components/EmployeeCard";
+import axios from "axios";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SearchResultContainer />;
 }
 
 export default App;
